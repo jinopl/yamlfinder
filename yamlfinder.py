f2e3a55c18e66ac4817f1c8a1780eaab46ecf47a
JINO <jinojp123@gmail.com>
2023-08-04 15:25:27 +0530

    Adding yaml finder python file

---
+++ yamlfinder.py
@@ -0,0 +1,37 @@
+import argparse
+from datetime import datetime
+
+
+def opfilename():
+   now = datetime.now()
+   return 'file' + now.strftime("%d%m%Y%H%M") + '.txt'
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ifile',type=str,dest='ifile',help="name of input file", required=True)
+parser.add_argument('--ofile',type=str,dest='ofile',help="name of output file")
+args = parser.parse_args()
+
+if not args.ofile:
+    ofile = opfilename()
+else:
+    ofile = args.ofile
+
+ifile = args.ifile
+
+with open(ifile) as myfile:
+   content_list = myfile.readlines()
+
+
+yamlfiles = []
+
+
+for f in content_list:
+   if f.strip().endswith(('.yaml','.yml')):
+    yamlfiles.append(f.strip())
+
+with open(ofile, 'w') as f:
+    for line in yamlfiles:
+        f.write(line)
+        f.write('\n')
