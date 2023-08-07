cdb08ab3dddaaa3595b5abd549ff9d6c81c6b893
JINO <jinojp123@gmail.com>
2023-08-07 12:11:03 +0530

    disable

--- yamlfinder.py
+++ yamlfinder.py
@@ -8,32 +8,32 @@ def opfilename():
 
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--ifile',type=str,dest='ifile',help="name of input file", required=True)
-parser.add_argument('--ofile',type=str,dest='ofile',help="name of output file")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--ifile',type=str,dest='ifile',help="name of input file", required=True)
+# parser.add_argument('--ofile',type=str,dest='ofile',help="name of output file")
+# args = parser.parse_args()
 
-if not args.ofile:
-    ofile = opfilename()
-else:
-    ofile = args.ofile
+# if not args.ofile:
+#     ofile = opfilename()
+# else:
+#     ofile = args.ofile
 
-ifile = args.ifile
+# ifile = args.ifile
 
-#sys.exit(1)
+# #sys.exit(1)
 
-# with open(ifile) as myfile:
-#    content_list = myfile.readlines()
-
-
-yamlfiles = []
+# # with open(ifile) as myfile:
+# #    content_list = myfile.readlines()
 
 
-# for f in content_list:
-#    if f.strip().endswith(('.yaml','.yml')):
-    # yamlfiles.append(f.strip())
+# yamlfiles = []
 
-# with open(ofile, 'w') as f:
-    # for line in yamlfiles:
-        # f.write(line)
-        # f.write('\n')
+
+# # for f in content_list:
+# #    if f.strip().endswith(('.yaml','.yml')):
+#     # yamlfiles.append(f.strip())
+
+# # with open(ofile, 'w') as f:
+#     # for line in yamlfiles:
+#         # f.write(line)
+#         # f.write('\n')
